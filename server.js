0ff562e459ca75275c7d16ac7a50a21227ee03f3
henrygustavo <henrygustavof@gmail.com>
2016-09-25 12:21:56 -0500

    ini config

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+
+//APP CONFIGURATION
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+var apiRouter = express.Router();
+
+app.use('/api', apiRouter);
+
+app.set('port', 9090);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+app.listen(app.get('port'));
+
+console.log("here we go");
